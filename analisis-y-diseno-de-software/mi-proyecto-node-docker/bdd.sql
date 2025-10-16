5a1d4bf13f8e22f8104bd2f550a6f2e8a344d0a4
Pepina29 <jpinom@usm.cl>
2025-10-15 22:57:23 -0300

    Create bdd.sql

---
+++ analisis-y-diseno-de-software/mi-proyecto-node-docker/bdd.sql
@@ -0,0 +1,15 @@
+CREATE TABLE usuario (
+    rut_cliente INT PRIMARY KEY,
+    nombre_cliente VARCHAR(50),
+    apellido_cliente VARCHAR(50),
+    contraseña TEXT  -- hay que hashearla
+);
+
+CREATE TABLE solicitud_prestamo (
+    id_solicitud INT PRIMARY KEY,
+    rut_cliente INT,
+    monto_cliente INT,
+    cant_cuotas INT,
+    FOREIGN KEY (rut_cliente) REFERENCES usuario(rut_cliente)
+        ON DELETE CASCADE 
+);
