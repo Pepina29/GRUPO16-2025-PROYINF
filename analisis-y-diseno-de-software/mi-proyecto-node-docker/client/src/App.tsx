4104503c0e797ae09edb1d2f58e0fd785e7fb725
kilepeo <kyleeeowo@gmail.com>
2025-11-28 18:19:53 -0300

    actualizado app 2 pag nuevas

--- analisis-y-diseno-de-software/mi-proyecto-node-docker/client/src/App.tsx
+++ analisis-y-diseno-de-software/mi-proyecto-node-docker/client/src/App.tsx
@@ -7,7 +7,8 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import LoanApplication from "./pages/LoanApplication";
 import Information from "./pages/Information";
-import Perfil from "@/pages/Perfil";
+import EvalRiesgo from "./pages/EvalRiesgo";
+import EvalRiesgoInt from "./pages/EvalRiesgoInt";
 
 const queryClient = new QueryClient();
 
@@ -21,7 +22,8 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/solicitar-prestamo" element={<LoanApplication />} />
           <Route path="/informacion" element={<Information />} />
-          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/eval-riesgo" element={<EvalRiesgo />} />
+          <Route path="/eval-riesgo-int" element={<EvalRiesgoInt />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
